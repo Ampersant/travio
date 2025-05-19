c0fb7bc23a11947072a8485fd0eeac4bb16c4520
Ampersant <pika4yxa1337@gmail.com>
2025-05-19 10:58:15 +0200

    feat(frontend): add index page with styles and assets

--- travio-app/routes/web.php
+++ travio-app/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\RenderController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [RenderController::class, 'index']);
